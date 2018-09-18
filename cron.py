83df71a4d673df40d832d24565e9a513d3173656
Jon Bulica <jonbulica99@gmail.com>
2018-09-18 13:04:50 +0200

    cron has now the full path to the interpreter and modules

--- cron.py
+++ cron.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-import settings
-
 __author__ = 'jbu'
+import os
+import sys
+import settings
 from crontab import CronTab
 
 
@@ -10,7 +11,9 @@ class Cron:
     def __init__(self, command=settings.CRON_DEFAULT_COMMAND, time=settings.CRON_DEFAULT_TIME, logger=None):
         self.time = time
         self.command = command
-        self.comment = 'Monitoring_{}'.format(self.command)
+        # command must contain the python interpreter and the full path to the module
+        self.full_command = '{} {}/{}'.format(sys.executable, os.path.dirname(__file__), command)
+        self.comment = 'Monitoring_{}'.format(command)
         self.logger = logger
         # Linux-only, loads the crontab from the $USER variable
         self.crontab = CronTab(user=True)
@@ -18,13 +21,13 @@ class Cron:
 
     def create_job(self, job=None):
         if job is None:
-            job = self.crontab.new(command=self.command, comment=self.comment)
+            job = self.crontab.new(command=self.full_command, comment=self.comment)
 
         job.setall(self.time)
 
         if job.is_valid():
             existing_jobs = self.crontab.find_comment(self.comment)
-            if sum(1 for _ in existing_jobs) > 0:
+            if sum(1 for _ in existing_jobs) > 1:
                 self.logger.info("Cronjob {} at interval {} already exists in {}'s crontab and won't be duplicated"
                                  .format(self.command, self.time, self.crontab.user))
                 return
